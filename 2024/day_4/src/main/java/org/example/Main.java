0f272b01db2400d5343b35de1dd34f4afcee73fa
Mats Fredriksson <mats.fredriksson@instabee.com>
2024-12-21 12:43:34 +0100

    2024 Day 4, init

---
+++ 2024/day_4/src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    private static String readFile(String fileName) throws IOException {
+        return Files.readAllLines(Path.of(fileName)).stream().collect(Collectors.joining());
+    }
+
+
+    public static void main(String[] args) {
+//        String file = "src/main/resources/input";
+//        String file = "src/main/resources/test.txt";
+        String file = "src/main/resources/test2.txt";
+        try {
+            var content = readFile(file);
+            System.out.println(content);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
